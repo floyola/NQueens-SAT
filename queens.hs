eac8ec28c870742066e33597fd2a117815a40c6a
Luis Fernando Loyola Cruz <fndloyolacz@gmail.com>
2020-04-11 12:43:08 -0500

    Refactor: Cleaning code

--- queens.hs
+++ queens.hs
@@ -5,26 +5,32 @@ import Propositional
 ----------------------------------------
 ----------------------------------------
 
-exactlyOne :: [Literal] -> Formula
-exactlyOne vars = (foldr1 (:&) . map (\(p,q) -> Not(p) :| Not(q)) $  pairs vars)
-
+-- Returns all possible pairs without symmetries
 pairs :: [a] -> [(a, a)]
 pairs [] = []
 pairs (x : xs) = map ((,) x) xs ++ pairs xs
 
-diagonals :: Int -> [[Literal]]
-diagonals n = [[ Var $ chessSquare x y | x <- [1..n], y <- [1..n], x-y == t] | t <- [-bound..bound]]
+-- Given a set of variables construct a formula that corresponds to the *maxOne* SAT constraint.
+maxOne :: [Literal] -> Formula
+maxOne = foldr1 (:&) . map (\(p,q) -> Not(p) :| Not(q)) .  pairs
+
+-- Returns lists of variables that correspond to the diagonals of the chessboard
+encodeDiagonals :: Int -> [[Literal]]
+encodeDiagonals n = [[ Var(chessSquare x y) | x <- [1..n], y <- [1..n], x-y == t] | t <- [-bound..bound]]
   where bound = n-2
 
-antiDiagonals :: Int -> [[Literal]]
-antiDiagonals n = [[ Var $ chessSquare x y | x <- [1..n], y <- [1..n], x+y == t] | t <- [3..bound]]
+-- Returns lists of variables that correspond to the anti-diagonals of the chessboard
+encodeAntiDiagonals :: Int -> [[Literal]]
+encodeAntiDiagonals n = [[ Var(chessSquare x y) | x <- [1..n], y <- [1..n], x+y == t] | t <- [3..bound]]
   where bound = 2*n-1
 
-columns :: Int -> [[Literal]]
-columns n = [[Var $ chessSquare x y | x <- [1..n]] | y <- [1..n]]
+-- Returns lists of variables that correspond to the columns of the chessboard
+encodeColumns :: Int -> [[Literal]]
+encodeColumns n = [[Var(chessSquare x y) | x <- [1..n]] | y <- [1..n]]
 
-rows :: Int -> [[Literal]]
-rows n = [[Var $ chessSquare x y | y <- [1..n]] | x <- [1..n]]
+-- Returns lists of variables that correspond to the rows of the chessboard
+encodeRows :: Int -> [[Literal]]
+encodeRows n = [[Var(chessSquare x y) | y <- [1..n]] | x <- [1..n]]
 
 chessSquare :: Int -> Int -> String
 chessSquare n m = "square_" ++ show n ++ "_" ++ show m
@@ -32,9 +38,17 @@ chessSquare n m = "square_" ++ show n ++ "_" ++ show m
 ----------------------------------------
 ----------------------------------------
 
+-- Encode the NQueens Problem into a Propositional Formula
 queensToSat :: Int -> Formula
-queensToSat n = foldr1 (:&) columnas :& (foldr1 (:&) renglones) :& (foldr1 (:&) diagonales) :& foldr1 (:&) atLeast
-  where columnas = map exactlyOne $ columns n
-        atLeast = map (foldr1 (:|)) $ columns n
-        renglones = map exactlyOne $ rows n
-        diagonales = (map exactlyOne $ diagonals n) ++ (map exactlyOne $ antiDiagonals n)
+queensToSat n = foldr1 (:&) columns
+                :& foldr1 (:&) rows
+                :& foldr1 (:&) diagonals
+                :& foldr1 (:&) antiDiagonals
+                :& foldr1 (:&) atLeastOne
+
+          where columns = map maxOne $ encodeColumns n
+                rows = map maxOne $ encodeRows n
+                diagonals = map maxOne $ encodeDiagonals n
+                antiDiagonals = map maxOne $ encodeAntiDiagonals n
+                -- We need to guarantee that there'll be at at least one queen per column
+                atLeastOne = map (foldr1 (:|)) $ encodeColumns n

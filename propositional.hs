2231334e5a9de62dc9538d14a4dec79e5ef8b025
Luis Fernando Loyola Cruz <fndloyolacz@gmail.com>
2020-04-11 12:16:38 -0500

    Clean code

--- propositional.hs
+++ propositional.hs
@@ -4,39 +4,42 @@ module Propositional where
 
 import Data.List(nub)
 
-data Formula = Variable String
+----------------------------------------
+----------------------------------------
+
+
+data Formula = Var String
              | Not Formula
-             | Formula :&Formula
-             | Formula :|Formula
-             | Formula :=>Formula
-             | Formula :<=>Formula
+             | Formula :& Formula
+             | Formula :| Formula
+             | Formula :=> Formula
+             | Formula :<=> Formula
                  deriving (Ord, Eq)
 
-type Literal = Formula
+instance Show Formula where
+       show (Var v) = show v
+       show (Not p) = "¬"    ++ show p
+       show (p :& q) = "(" ++ show p ++ " ∧ "     ++ show q ++ ")"
+       show (p :| q) = "(" ++ show p ++ " ∨ "     ++ show q ++ ")"
+       show (p :=> q) = "(" ++ show p ++ " → "     ++ show q ++ ")"
+       show (p :<=> q) = "(" ++ show p ++ " ⟷ "      ++ show q ++ ")"
 
+type Literal = Formula
 type Clausula = [Literal]
 
 infixl 9 :&
-
 infixl 9 :|
-
 infixr 7 :=>
-
 infixl 8 :<=>
 
-instance Show Formula where
-        show (Variable v) = show v
-        show (Not p) = "¬"    ++ show p
-        show (p :& q) = "(" ++ show p ++ " ∧ "     ++ show q ++ ")"
-        show (p :| q) = "(" ++ show p ++ " ∨ "     ++ show q ++ ")"
-        show (p :=> q) = "(" ++ show p ++ " → "     ++ show q ++ ")"
-        show (p :<=> q) = "(" ++ show p ++ " ⟷ "      ++ show q ++ ")"
+----------------------------------------
+----------------------------------------
 
--- Convierte una fórmula de la Lógica Proposisional en
--- Forma Normal Negativa.
+
+-- Transform a formula p into Negative Normal Form
 fnn :: Formula -> Formula
-fnn expr@(Variable _) = expr
-fnn expr@(Not (Variable _)) = expr
+fnn expr@(Var _) = expr
+fnn expr@(Not (Var _)) = expr
 fnn (Not (Not expr)) = expr
 
 fnn (exp1 :& exp2) = fnn exp1 :& fnn exp2
@@ -57,8 +60,7 @@ fnn (Not (exp1 :<=> exp2))
         b = Not exp1 :| Not exp2
       in fnn $ a :& b
 
--- Convierte una fórmula de la Lógica Proposisional en
--- Forma Normal Conjuntiva.
+-- Transform a formula p into Conjunctive Normal Form
 fnc :: Formula -> Formula
 fnc = fnc' . fnn
   where fnc' :: Formula -> Formula
@@ -71,41 +73,9 @@ fnc = fnc' . fnn
         dist e1 (e21 :& e22) = (dist e1 e21) :& (dist e1 e22)
         dist e1 e2 = e1 :| e2
 
--- Dada una fórmula proposisional en forma normal conjuntiva
--- devuelve una lista con las claúsulas que la forman.
-clausulas :: Formula -> Clausula
-clausulas (p :| q) = [p :| q]
-clausulas (p :& q) = clausulas p ++ clausulas q
-clausulas p = [p]
-
--- Dada una claúsula regresa la lista de las literales que la conforma.
-literales :: Formula -> Clausula
-literales var@(Variable _) = [var]
-literales negVar@(Not _) = [negVar]
-literales (p :| q) = literales p ++ literales q
-
--- Dada una fórmula regresa la lista de las variables que la conforma.
+-- Given a formula returns the list of variables that compose it.
 variables :: Formula -> Clausula
-variables var@(Variable _) = [var]
+variables var@(Var _) = [var]
 variables (Not x) = variables x
 variables (p :| q) = nub $ variables p ++ variables q
 variables (p :& q) = nub $ variables p ++ variables q
-
--- Verifica que una clausula sea unitaria.
-isUnit :: Clausula -> Bool
-isUnit [p] = True
-isUnit _ = False
-
--- Verifica que una literal este negada.
-isNegLiteral :: Literal -> Bool
-isNegLiteral (Not _) = True
-isNegLiteral _ = False
-
--- Verifica que una clausula sea positiva.
-isPositive :: Clausula -> Bool
-isPositive c = all (not . isNegLiteral) c
-
---Dada una Literal regresa su complemento.
-complemento :: Literal -> Literal
-complemento var@(Variable _) = Not var
-complemento (Not (Variable f)) = Variable f
--- queens.hs
+++ queens.hs
@@ -2,21 +2,8 @@ module Queens where
 
 import Propositional
 
-p :: Formula
-p = Variable "p"
-q :: Formula
-q = Variable "q"
-
-r :: Formula
-r = Variable "r"
-
-queensToSat :: Int -> Formula
-queensToSat n = foldr1 (:&) columnas :& (foldr1 (:&) renglones) :& (foldr1 (:&) diagonales) :& foldr1 (:&) atLeast
-  where columnas = map exactlyOne $ columns n
-        atLeast = map (foldr1 (:|)) $ columns n
-        renglones = map exactlyOne $ rows n
-        diagonales = (map exactlyOne $ diagonals n) ++ (map exactlyOne $ antiDiagonals n)
-
+----------------------------------------
+----------------------------------------
 
 exactlyOne :: [Literal] -> Formula
 exactlyOne vars = (foldr1 (:&) . map (\(p,q) -> Not(p) :| Not(q)) $  pairs vars)
@@ -26,15 +13,28 @@ pairs [] = []
 pairs (x : xs) = map ((,) x) xs ++ pairs xs
 
 diagonals :: Int -> [[Literal]]
-diagonals n = [[ Variable $ "coord_" ++ show x ++ show y | x <- [1..n], y <- [1..n], x-y == t] | t <- [-bound..bound]]
+diagonals n = [[ Var $ chessSquare x y | x <- [1..n], y <- [1..n], x-y == t] | t <- [-bound..bound]]
   where bound = n-2
 
 antiDiagonals :: Int -> [[Literal]]
-antiDiagonals n = [[ Variable $ "coord_" ++ show x ++ show y | x <- [1..n], y <- [1..n], x+y == t] | t <- [3..bound]]
+antiDiagonals n = [[ Var $ chessSquare x y | x <- [1..n], y <- [1..n], x+y == t] | t <- [3..bound]]
   where bound = 2*n-1
 
 columns :: Int -> [[Literal]]
-columns n = [[Variable $ "coord_" ++ show x ++ show y | x <- [1..n]] | y <- [1..n]]
+columns n = [[Var $ chessSquare x y | x <- [1..n]] | y <- [1..n]]
 
 rows :: Int -> [[Literal]]
-rows n = [[Variable $ "coord_" ++ show x ++ show y | y <- [1..n]] | x <- [1..n]]
+rows n = [[Var $ chessSquare x y | y <- [1..n]] | x <- [1..n]]
+
+chessSquare :: Int -> Int -> String
+chessSquare n m = "square_" ++ show n ++ "_" ++ show m
+
+----------------------------------------
+----------------------------------------
+
+queensToSat :: Int -> Formula
+queensToSat n = foldr1 (:&) columnas :& (foldr1 (:&) renglones) :& (foldr1 (:&) diagonales) :& foldr1 (:&) atLeast
+  where columnas = map exactlyOne $ columns n
+        atLeast = map (foldr1 (:|)) $ columns n
+        renglones = map exactlyOne $ rows n
+        diagonales = (map exactlyOne $ diagonals n) ++ (map exactlyOne $ antiDiagonals n)
